df29f7d44f4661b5f95f9a7dca4741286b96f9b8
Ayaan-Ramin <ayaanr@nycstudents.net>
2025-11-26 13:39:53 -0500

    Add test cases for WhileLoops methods

--- src/main/java/com/example/project/WhileLoopsRunner.java
+++ src/main/java/com/example/project/WhileLoopsRunner.java
@@ -4,6 +4,20 @@ package com.example.project;
 public class WhileLoopsRunner{
 
     public static void main(String[] args) {
-        //manually test here 
+
+        int n = WhileLoops.countEvensUpTo(10);
+        System.out.println(n);
+        System.out.println(WhileLoops.countEvensUpTo(20));
+
+        System.out.println(WhileLoops.digitSum(4098));
+        System.out.println(WhileLoops.countDownBy(20, 3));
+
+        System.out.println(WhileLoops.weeksToReachGoal(0, 50, 200));
+        System.out.println(WhileLoops.weeksToReachGoal(100, 20, 110));
+        System.out.println(WhileLoops.weeksToReachGoal(500, 100, 400));
+        System.out.println(WhileLoops.weeksToReachGoal(10, 10, 40));
+        System.out.println(WhileLoops.minutesUntilDead(100, 10));
+
+       
     }
 }
